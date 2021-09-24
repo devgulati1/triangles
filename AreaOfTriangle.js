95f000531d701ff1c0ca578368b205427e9679d6
Dev Gulati <gulatidev84@gmail.com>
2021-09-24 15:01:50 +0530

    done

--- AreaOfTriangle.js
+++ AreaOfTriangle.js
@@ -12,10 +12,17 @@ let calArea=(a,b)=>{
 
 }
 let ShowArea=()=>{
-    let ans=calArea(Number(inputs[0].value),Number(inputs[1].value));
+    let a=Number(inputs[0].value)
+    let b=Number(inputs[1].value)
+    console.log(` a is ${a} and b is ${b}`)
+    if(a<0 || b<0){
+        output.innerHTML="please enter positive values"
+       }else{
+    let ans=calArea(a,b);
    
     output.innerText=`The Area Of Triangle is ${ans}`;
 
+}
 }
 
 button.addEventListener("click",ShowArea)
--- calculateHyp.js
+++ calculateHyp.js
@@ -5,19 +5,31 @@ let output=document.querySelector(".output")
 
 
 let sumOfSides=(a,b)=>{
+    
+    
+
+   
     let sum=0;
     sum=a*a+b*b;
     return sum;
-
 }
 
+
+
 let calcH=()=>{
-    let sum=sumOfSides(Number(inputs[0].value),Number(inputs[1].value));
-   
+    let a=Number(inputs[0].value)
+    let b=Number(inputs[1].value)
+    console.log(` a is ${a} and b is ${b}`)
+    if(a<0 || b<0){
+        output.innerHTML="please enter positive values"
+       }else{
+    
+    let sum=sumOfSides(a,b);
+       
    
     let lengthOfH=Math.sqrt(sum);
     output.innerHTML=`The Hypotenuese  Length is ${lengthOfH}`
-
+       }
 
 
 }
